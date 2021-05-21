f80c20c4515ce0243d1f661b8a7c0a55880d2e60
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2021-05-20 22:09:56 -0300

    exemplo simples entity framework

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext:DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> MyProperty { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData");
+        }
+
+
+    }
+}
