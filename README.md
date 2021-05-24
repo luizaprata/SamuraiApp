a71c6f7201582d386983883090c191eeb4cafa88
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2021-05-24 20:53:11 -0300

    migration m2m

---
+++ README.md
@@ -0,0 +1,8 @@
+### Comandos aprendidos
+
+- `Add-Migration [arguments]` ou `dotnet ef migrations add [arguments] [options]`
+  - Cria uma migration nova com as alteracoes realizadas ex: `Add-Migration m2mpayload`
+- `Get-Migration` ou `dotnet ef migrations list`
+  - Lista as migrations realizadas
+- `update database`
+  - roda as migrations
---
+++ SamuraiApp.Data/Migrations/20210524233752_m2mpayload.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class m2mpayload : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BattleSamurai");
+
+            migrationBuilder.CreateTable(
+                name: "SamuraiBattle",
+                columns: table => new
+                {
+                    SamuraiId = table.Column<int>(type: "int", nullable: false),
+                    BattleId = table.Column<int>(type: "int", nullable: false),
+                    DateJoined = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getDate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SamuraiBattle", x => new { x.BattleId, x.SamuraiId });
+                    table.ForeignKey(
+                        name: "FK_SamuraiBattle_Battles_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SamuraiBattle_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SamuraiBattle_SamuraiId",
+                table: "SamuraiBattle",
+                column: "SamuraiId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SamuraiBattle");
+
+            migrationBuilder.CreateTable(
+                name: "BattleSamurai",
+                columns: table => new
+                {
+                    BattlesId = table.Column<int>(type: "int", nullable: false),
+                    SamuraisId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BattleSamurai", x => new { x.BattlesId, x.SamuraisId });
+                    table.ForeignKey(
+                        name: "FK_BattleSamurai_Battles_BattlesId",
+                        column: x => x.BattlesId,
+                        principalTable: "Battles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BattleSamurai_Samurais_SamuraisId",
+                        column: x => x.SamuraisId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BattleSamurai_SamuraisId",
+                table: "BattleSamurai",
+                column: "SamuraisId");
+        }
+    }
+}
