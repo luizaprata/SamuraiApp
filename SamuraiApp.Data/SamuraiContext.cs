8f4c90b2bff001dc2388436cd17ea5769cef4b59
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2021-05-25 20:58:51 -0300

    one-to-one!

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -9,6 +9,7 @@ namespace SamuraiApp.Data
         public DbSet<Battle> Battles { get; set; }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Horse> Horses { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
